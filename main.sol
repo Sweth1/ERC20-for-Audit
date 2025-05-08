86d5da98425ec5798e409fc2d5a65fb738d328ad
Sweth1 <fedderiko2122@gmail.com>
2025-05-09 05:49:36 +0800

    main.sol

--- main.sol
+++ main.sol
@@ -1,1 +1,57 @@
-# ERC20-for-Audit 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract tires {
+    string public name = "1010tires";  
+    string public symbol = "TRS";     
+    uint8 public decimals = 18;       
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply = 1000000 * 10**decimals; 
+
+    constructor() {
+        _balances[msg.sender] = _totalSupply;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(_balances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
+
+        _balances[msg.sender] -= amount; 
+        _balances[recipient] += amount; 
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[msg.sender][spender] = amount; // Set allowance
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
+        require(_allowances[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+
+        _balances[sender] -= amount; 
+        _balances[recipient] += amount; 
+        _allowances[sender][msg.sender] -= amount; 
+        return true;
+    }
+}
